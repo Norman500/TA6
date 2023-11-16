2fe49b735af6459b86ffd3e76dc19e3d2dd55bd6
huming.hm <huming@weidian.com>
2023-11-16 09:39:45 +0800

    mod

--- src/main/java/BaseController.java
+++ src/main/java/BaseController.java
@@ -7,8 +7,10 @@
 public class BaseController {
 
     // Add Code here
+    // Test Git Add
     public static void main(String[] args) {
         System.out.print("dev branch a");
         System.out.println("test add");
+        System.out.println("test stash");
     }
 }
